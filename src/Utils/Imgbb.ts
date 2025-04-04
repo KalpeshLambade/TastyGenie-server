9d0bd2b434b92f6f52eed2cd85a7f3ddb8632019
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2025-04-04 10:08:13 +0530

    debug prod

--- src/Utils/Imgbb.ts
+++ src/Utils/Imgbb.ts
@@ -8,6 +8,10 @@ export class Imgbb{
 
     public static async uploadImage(image: File | Blob | string, name?: string) {
         try {
+            console.log("IMGBB Key:", Config.configKeys.imgbb);
+            console.log(typeof FormData);
+            console.log(typeof Blob);
+
             const formData = new FormData();
             formData.append('key', Config.configKeys.imgbb);
             formData.append('expiration', Imgbb.EXPIRATION_TIME.toString()); 
@@ -21,7 +25,10 @@ export class Imgbb{
                 if (image.size > Imgbb.MAX_SIZE_BYTES) {
                     throw new Error(`Image size exceeds ${Imgbb.MAX_SIZE_MB} MB limit.`);
                 }
-                formData.append('image', image, name || 'image.jpg');
+                // formData.append('image', image, name || 'image.jpg');
+
+                const base64String = await Imgbb.convertToBase64(image);
+                formData.append('image', base64String);
             }
             const response = await axios.post('https://api.imgbb.com/1/upload', formData, {
                 headers: {
@@ -31,7 +38,19 @@ export class Imgbb{
             return response.data?.data?.display_url;
         } catch (error) {
             console.error('Image upload failed:', error);
-            throw error; 
+            // throw error; 
         }
+    }
+
+    private static convertToBase64(file: File | Blob): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                const base64String = (reader.result as string).split(',')[1]; 
+                resolve(base64String);
+            };
+            reader.onerror = (error) => reject(error);
+        });
     }
 }
